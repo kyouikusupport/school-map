14a90623ee7df78418e04767deb1d8c38730ecc0
kyouikusupport <185699892+kyouikusupport@users.noreply.github.com>
2025-05-24 23:34:28 +0900

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,10 @@
+﻿// js/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB-DFpZM6E4EbXI5fAUC9H9w1cwVwTrU8M",
+  authDomain: "school-map2025.firebaseapp.com",
+  databaseURL: "https://school-map2025-default-rtdb.firebaseio.com",
+  projectId: "school-map2025",
+  storageBucket: "school-map2025.firebasestorage.app",
+  messagingSenderId: "118778734093",
+  appId: "1:118778734093:web:b4fadd9251601c65c45744"
+};
